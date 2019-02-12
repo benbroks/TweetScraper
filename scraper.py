6e1c342921f96b074ad76dc3820b3acfbdb7ae38
Ben Brooks <bjbrooks@usc.edu>
2019-02-12 10:08:40 -0800

    first commit

---
+++ scraper.py
@@ -0,0 +1,19 @@
+import subprocess
+
+republicans  = open('data/r.txt', 'r')
+rList = republicans.readlines()
+rCommands = open('rCommands.txt', 'w')
+democrats  = open('data/d.txt', 'r')
+dList = democrats.readlines()
+dCommands = open('dCommands.txt', 'w')
+
+for i in range(len(rList)):
+    rList[i] = rList[i][:-1]
+    name_of_file = 'r/' + rList[i] + '.csv'
+    rCommands.write('twitterscraper -u ' + rList[i] + ' -l 500 -c -o ' + name_of_file + '\n')
+
+for i in range(len(dList)):
+    dList[i] = dList[i][:-1]
+    name_of_file = 'data/d/' + dList[i] + '.csv'
+    dCommands.write('twitterscraper -u ' + dList[i] + ' -l 500 -c -o ' + name_of_file + '\n')
+
